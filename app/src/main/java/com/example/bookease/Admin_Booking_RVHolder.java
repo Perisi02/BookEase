77a44fa9811b2717a1a55dc0b61058ff863596c9
Leati Perise <Perisi02@myunitec.ac.nz>
2026-05-30 17:19:10 +1200

    Create RVHolder for admin booking recycler view

---
+++ app/src/main/java/com/example/bookease/Admin_Booking_RVHolder.java
@@ -0,0 +1,35 @@
+package com.example.bookease;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Admin_Booking_RVHolder extends RecyclerView.ViewHolder {
+    TextView resId;
+    TextView name;
+    TextView date;
+    TextView time;
+    TextView guests;
+    TextView status;
+    Button btnAction;
+    Button btnCancel;
+    Button btnConfirm;
+
+    public Admin_Booking_RVHolder(@NonNull View itemView) {
+        super(itemView);
+
+        resId = itemView.findViewById(R.id.tv_resId);
+        name = itemView.findViewById(R.id.tv_name);
+        date = itemView.findViewById(R.id.tv_date);
+        time = itemView.findViewById(R.id.tv_time);
+        guests = itemView.findViewById(R.id.tv_guests);
+        status = itemView.findViewById(R.id.tv_status);
+
+        btnAction = itemView.findViewById(R.id.btn_action);
+        btnCancel = itemView.findViewById(R.id.btn_cancelBooking);
+        btnConfirm = itemView.findViewById(R.id.btn_confirmBooking);
+    }
+}
